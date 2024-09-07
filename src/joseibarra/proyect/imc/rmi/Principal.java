12f8ca18b42d6291072f0d9a0d57b5a5eebe3279
unknown <jcibarrah1423@gmail.com>
2024-09-06 20:36:48 -0500

    RMIClient

---
+++ src/joseibarra/proyect/imc/rmi/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package joseibarra.proyect.imc.rmi;
+
+import joseibarra.proyect.imc.rmi.vistas.VentanaPrincipal;
+
+/**
+ *
+ * @author Jose Carlos Ibarra H
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+    
+}
